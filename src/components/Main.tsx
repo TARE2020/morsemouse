c1ca8ba28eea71731bc0184a5481a3c1650a6a8b
tobiaserich <56834185+tobiaserich@users.noreply.github.com>
2020-02-11 22:52:41 +0100

    Add text to morse function (#9)

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -28,16 +28,71 @@ const Label = styled.label`
   height: 40%;
 `;
 const Main = () => {
+  const [inputText, setInputText] = React.useState("");
+  const [resultText, setResultText] = React.useState("");
+
+  const characterToCode: any = {
+    A: "·−",
+    B: "−···",
+    C: "−·−·",
+    D: "−··",
+    E: "·",
+    F: "··−·",
+    G: "−−·",
+    H: "····",
+    I: "··",
+    J: "·−−−",
+    K: "−·−",
+    L: "·−··",
+    M: "−−",
+    N: "−·",
+    O: "−−−",
+    P: "·−−·",
+    Q: "−−·−",
+    R: "·−·",
+    S: "···",
+    T: "−",
+    U: "··−",
+    V: "···−",
+    W: "·−−",
+    X: "−··−",
+    Y: "−·−−",
+    Z: "−−··"
+  };
+
+  const textToMorseCode = () => {
+    const inputValue = inputText.split("");
+    let resultCharacters: any = [];
+    inputValue.map((character: string) => {
+      const upperCaseCharacter: string = character.toUpperCase();
+      const preparedCharacter: number = upperCaseCharacter.charCodeAt(0);
+      if (character === " ") {
+        resultCharacters.push("/");
+      } else if (preparedCharacter > 64 && preparedCharacter < 91) {
+        resultCharacters.push(characterToCode[upperCaseCharacter]);
+      }
+      const resultText = resultCharacters.join(" ");
+      setResultText(resultText);
+      return null;
+    });
+  };
+
+  React.useEffect(() => textToMorseCode(), [inputText]);
   return (
     <Container>
       <Label>
         Text
-        <Textarea />
+        <Textarea
+          value={inputText}
+          onChange={event => {
+            setInputText(event.target.value);
+          }}
+        />
       </Label>
 
       <Label>
         Morse
-        <Textarea />
+        <Textarea value={resultText} />
       </Label>
     </Container>
   );

fac61c43d842a19d5f51226bbcf1e28c3ad36213
tobiaserich <56834185+tobiaserich@users.noreply.github.com>
2020-02-13 20:12:10 +0100

    Add SOS function and button bar as footer (#14)
    * Add SOS function and button bar as footer
    
    * Delete console log

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,22 @@ import React from "react";
 import GlobalStyles from "./GlobalStyles";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import Footer from "./components/Footer";
+
 const App = () => {
+  const [isEmergency, setIsEmergency] = React.useState(false);
   return (
     <>
       <GlobalStyles />
       <Header />
-      <Main />
+      <Main
+        isEmergency={isEmergency}
+        handleEmergency={(bool: boolean) => setIsEmergency(bool)}
+      />
+      <Footer
+        isEmergency={isEmergency}
+        toggleEmergency={() => setIsEmergency(!isEmergency)}
+      />
     </>
   );
 };
---
+++ src/components/Footer.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "@emotion/styled";
+type FooterProps = {
+  isEmergency: boolean;
+  toggleEmergency: any;
+};
+const FooterBar = styled.div`
+  display: flex;
+  width: 100%;
+  height: 10%;
+`;
+
+const Button = styled.button`
+  flex-grow: 1;
+  height: 100%;
+  background-color: #545454;
+  border: none;
+  color: #d3d3d3;
+  font-family: "Russo One";
+  font-size: 1.3rem;
+  text-shadow: 2px 2px 10px black;
+  border-right: 1px solid #d3d3d3;
+  border-left: 1px solid #d3d3d3;
+`;
+
+const Footer = ({ isEmergency, toggleEmergency }: FooterProps) => {
+  return (
+    <FooterBar>
+      <Button>PLAY</Button>
+      <Button onClick={toggleEmergency}>SOS</Button>
+    </FooterBar>
+  );
+};
+
+export default Footer;
